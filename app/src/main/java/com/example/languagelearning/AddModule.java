7e48748163f243cfd9db8b2729c92771fd93e2fe
Anastasia Fongrad <fongradnastya@gmail.com>
2023-05-26 13:24:16 +0700

    Added navigation

--- app/src/main/java/com/example/languagelearning/AddModule.java
+++ app/src/main/java/com/example/languagelearning/AddModule.java
@@ -2,7 +2,9 @@ package com.example.languagelearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AddModule extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class AddModule extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_module);
+        Button button = findViewById(R.id.exit_btn);
+        button.setOnClickListener(v -> openReminderAddActivity());
+    }
+    public void openReminderAddActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/languagelearning/MainActivity.java
+++ app/src/main/java/com/example/languagelearning/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.languagelearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.btn_add_reminder);
+        button.setOnClickListener(v -> openReminderAddActivity());
+    }
+
+    public void openReminderAddActivity() {
+        Intent intent = new Intent(this, AddModule.class);
+        startActivity(intent);
     }
 }
