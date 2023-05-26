a0c644d992019406499776c911065571e36fa215
Anastasia Fongrad <fongradnastya@gmail.com>
2023-05-26 14:18:43 +0700

    Added translation

---
+++ app/src/main/java/com/example/languagelearning/LearnModule.java
@@ -0,0 +1,43 @@
+package com.example.languagelearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LearnModule extends AppCompatActivity {
+
+    private boolean isHidden;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.isHidden = true;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_module);
+        Button button_exit = findViewById(R.id.exit);
+        button_exit.setOnClickListener(v -> exit());
+        Button button_translate = findViewById(R.id.translate);
+        button_translate.setOnClickListener(v -> showTranslation());
+    }
+
+    public void exit(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void showTranslation(){
+        TextView text = findViewById(R.id.translation);
+        Button button_translate = findViewById(R.id.translate);
+        if (this.isHidden){
+            text.setText("Translation");
+            button_translate.setText("Hide");
+        }
+        else{
+            text.setText("");
+            button_translate.setText("Translate");
+        }
+        this.isHidden = ! this.isHidden;
+    }
+}
--- app/src/main/java/com/example/languagelearning/MainActivity.java
+++ app/src/main/java/com/example/languagelearning/MainActivity.java
@@ -12,12 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.btn_add_reminder);
-        button.setOnClickListener(v -> openReminderAddActivity());
+        Button button_add = findViewById(R.id.btn_add_reminder);
+        button_add.setOnClickListener(v -> addNewModule());
+        Button button_learn = findViewById(R.id.learn1);
+        button_learn.setOnClickListener(v -> startLearning());
     }
 
-    public void openReminderAddActivity() {
+    public void addNewModule() {
         Intent intent = new Intent(this, AddModule.class);
+        startActivity(intent);
+    }
+
+    public void startLearning(){
+        Intent intent = new Intent(this, LearnModule.class);
         startActivity(intent);
     }
 }
