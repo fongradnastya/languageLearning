67cffc50d5dc16a0cfbfe825ccb5ab0bdf779d22
Anastasia Fongrad <fongradnastya@gmail.com>
2023-05-27 19:02:12 +0700

    Attempt to restart

--- app/src/main/java/com/example/languagelearning/AddModule.java
+++ app/src/main/java/com/example/languagelearning/AddModule.java
@@ -18,57 +18,29 @@ import java.io.InputStream;
 
 public class AddModule extends AppCompatActivity {
 
-    private EditText wordEditText;
-    private EditText translationEditText;
+    private LinearLayout mLayout;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_module);
-        Button button = findViewById(R.id.exit_btn);
-        Button submitButton = findViewById(R.id.btn_submit);
-        Button addCard = findViewById(R.id.add_card_btn);
-        addCard.setOnClickListener(v -> addNewCard());
-        button.setOnClickListener(v -> openReminderAddActivity());
-        submitButton.setOnClickListener(v -> serializeForm());
-        Context context = getApplicationContext();
-        AssetManager assets = context.getAssets();
-        // InputStream inputStream = assets.open("data.csv");
+        setContentView(R.layout.activity_main);
+        mLayout = (LinearLayout) findViewById(R.id.layout);
+        mButton = (Button) findViewById(R.id.new_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add two EditText views
+                mLayout.addView(createNewEditText());
+                mLayout.addView(createNewEditText());
+            }
+        });
     }
-    public void openReminderAddActivity() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    public void addNewCard() {
-        TextView title = findViewById(R.id.cards_title);
-        ConstraintLayout layout = findViewById(R.id.layout);
-        EditText editWord = new EditText(AddModule.this);
-        editWord.setLayoutParams(new ConstraintLayout.LayoutParams(0, ConstraintLayout.LayoutParams.WRAP_CONTENT));
-        editWord.setHint("The word");
-        editWord.setId(View.generateViewId());
-        layout.addView(editWord);
-        EditText editTranslation = new EditText(AddModule.this);
-        editTranslation.setLayoutParams(new ConstraintLayout.LayoutParams(0, ConstraintLayout.LayoutParams.WRAP_CONTENT));
-        editTranslation.setId(View.generateViewId());
-        editTranslation.setHint("The translation");
-        layout.addView(editTranslation);
-        ConstraintSet constraintSet1 = new ConstraintSet();
-        constraintSet1.clone(layout);
-        constraintSet1.connect(editWord.getId(), ConstraintSet.TOP, title.getId(), ConstraintSet.TOP);
-        constraintSet1.connect(editWord.getId(), ConstraintSet.START, layout.getId(), ConstraintSet.START);
-        constraintSet1.connect(editWord.getId(), ConstraintSet.END, layout.getId(), ConstraintSet.END);
-        constraintSet1.connect(editTranslation.getId(), ConstraintSet.TOP, editWord.getId(), ConstraintSet.TOP);
-        constraintSet1.connect(editTranslation.getId(), ConstraintSet.START, layout.getId(), ConstraintSet.START);
-        constraintSet1.connect(editTranslation.getId(), ConstraintSet.END, layout.getId(), ConstraintSet.END);
-        constraintSet1.applyTo(layout);
-
+    private EditText createNewEditText() {
+        final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final EditText editText = new EditText(this);
+        editText.setLayoutParams(lparams);
+        return editText;
     }
 
-
-    public void serializeForm(){
-        Word newWord;
-        String word = wordEditText.getText().toString();
-        String translation = translationEditText.getText().toString();
-    }
 }
